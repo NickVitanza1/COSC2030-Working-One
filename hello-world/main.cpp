0545fd5488aa26399d4c9d43e83df8abe64b7393
NickVitanza1 <nickvitanza@westernwyoming.edu>
2025-08-27 21:32:51 +0000

    Ready for grading

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Nicholas Vitanza
+// 8/27/25
 // Hello World Programming Project
 // COSC 2030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
