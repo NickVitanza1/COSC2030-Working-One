d5ce00b6b0916f30ab57f66d62a52911a6c33e43
NickVitanza1 <nickvitanza@westernwyoming.edu>
2025-08-29 00:27:23 +0000

    Ready for grading

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,32 @@
 //
-// Name
-// Date
+// Nichola Vitanza
+// 8/28/25
 // Backward String Programming Project
 // COSC 2030
 //
 
+#include <iostream>
+#include <cstring>   // for strlen
+using namespace std;
+
+// Function that prints a C-string backward
+void displayBackward(const char *str) {
+    int length = strlen(str); // find string length
+    for (int i = length - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    const int SIZE = 100;
+    char input[SIZE];
+
+    cout << "Enter a string: ";
+    cin.getline(input, SIZE); // read user input
+
+    cout << "String backward: ";
+    displayBackward(input);
+
+    return 0;
+}
